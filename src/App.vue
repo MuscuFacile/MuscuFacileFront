b502e81949d737082fff733ea67a37db0d90c0e0
CyprienF <floquet.cyprien@gmail.com>
2018-10-12 08:40:38 +0200

    Stylus configuration

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,6 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+@import '~@/style/main'
 </style>
