f4ea86cc1ba4a0ba6a69ae67bc97d5b87310c3bf
Cyprien FLOQUET <floquet.cyprien@gmail.com>
2018-10-19 10:51:52 +0200

    Chart.js integration (#1)

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    
+    <v-layout row wrap>
+      <v-flex xs12>
+        <chart></chart>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
+import Chart from '@/components/Chart.js'
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components: {
+    Chart
+  }
 }
 </script>
 
