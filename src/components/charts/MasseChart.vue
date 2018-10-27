cbc30c12dd5dbdc784d7edec048de2bfce1445f5
CyprienF <floquet.cyprien@gmail.com>
2018-10-27 20:37:35 +0200

    Dashboard UI improvements

---
+++ src/components/charts/MasseChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card height="100%">
+
+    <v-toolbar class="elevation-0">
+      <v-toolbar-title>Masse</v-toolbar-title>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <masse></masse>
+
+  </v-card>
+</template>
+
+<script>
+import Masse from './Masse.js'
+
+export default {
+  name: 'MasseChart',
+  components: {
+    Masse
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/charts/MusclesChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card height="100%">
+
+    <v-toolbar class="elevation-0">
+      <v-toolbar-title>Muscles</v-toolbar-title>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <muscles></muscles>
+
+  </v-card>
+</template>
+
+<script>
+import Muscles from './Muscles.js'
+
+export default {
+  name: 'MusclesChart',
+  components: {
+    Muscles
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
