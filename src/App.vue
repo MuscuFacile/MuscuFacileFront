6b976ff293524310e194a57a80e2a13c9d066ac0
CyprienF <floquet.cyprien@gmail.com>
2018-10-28 15:25:41 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
     <v-app>
-      <navbar></navbar>
+      <app-navbar></app-navbar>
       <v-container grid-list-md text-xs-center>
         <router-view/>
       </v-container>
+      <app-footer></app-footer>
     </v-app>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
+import AppNavbar from '@/components/Navbar'
+import AppFooter from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    AppNavbar,
+    AppFooter
   }
 }
 </script>
---
+++ src/components/dashboard/Imc.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card height="100%">
+    
+      <v-toolbar class="elevation-0">
+        <v-toolbar-title>IMC</v-toolbar-title>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-card-text>
+          
+        <v-progress-circular
+        :value="100"
+        :size="175"
+        :width="20"
+        color="green"
+      >
+        <h1>18</h1>
+      </v-progress-circular>
+
+      <h1 class="text-md-center">Corpulence normale</h1>
+
+      </v-card-text>
+    
+    </v-card>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
